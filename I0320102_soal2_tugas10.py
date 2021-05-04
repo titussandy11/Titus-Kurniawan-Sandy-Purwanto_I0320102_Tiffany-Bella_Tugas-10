7bd5762309cf25e12821309a80996351b8e21ecd
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-05-04 23:24:05 +0700

    Soal 2

--- I0320102_soal2_tugas10.py
+++ I0320102_soal2_tugas10.py
@@ -1,1 +1,19 @@
+print('SELAMAT DATANG DI PROGRAM INPUT BIODATA')
+print('===============================')
 
+#input dari user
+nama = input('Nama : ')
+kota_asal = input('asal : ')
+usia = input('usia : ')
+
+#format teks
+teks = 'Nama: {}\nasal: {}\nusia: {}'.format(nama, kota_asal, usia)
+
+#buka file
+file_bio = open('bio.txt', 'w')
+
+#tulis file
+file_bio.write(teks)
+
+#tutup file
+file_bio.close()
